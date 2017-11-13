baa3b30ce5702052ba4a3cb0a3c581fa6ed2c258
VCM admin account <vcm@vcm-2111.vm.duke.edu>
2017-11-12 19:28:40 -0500

    initial commits

---
+++ routes.py
@@ -0,0 +1,8 @@
+import sys
+import json
+import pymodm-example as pe 
+from flask import Flask, request, render_template
+
+if __name__ == '__main__':
+	app.run(debug=True, host='0.0.0.0', port=5000)
+
